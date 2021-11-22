b584e8d7a3c184b2113247117891e58354b9053d
Carlos Barros <carlos_dbs@hotmail.com>
2021-11-22 18:29:17 -0300

    Adiciona Inputs na pagina Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import Input from '../components/Input';
+
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <article>
+        <h2>Login</h2>
+        <Input
+          type="email"
+          dataTestId="email-input"
+          textLabel="Email:"
+        />
+        <Input
+          type="password"
+          dataTestId="password-input"
+          textLabel="Senha:"
+        />
+      </article>
+    );
   }
 }
 
