be11808845edee4094ecbf3c2c7926d4138815d6
Carlos Barros <carlos_dbs@hotmail.com>
2021-11-22 17:35:35 -0300

    Adiciona rootReducer e Provider no Index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 // import user from './user';
 // import wallet from './wallet';
+import { combineReducers } from 'redux';
 
-// Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+export default rootReducer = combineReducers({
+});
