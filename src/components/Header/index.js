54a53993e7e55ef7058579127e018f991b1eab22
Carlos Barros <carlos_dbs@hotmail.com>
2021-11-22 22:27:42 -0300

    Adiciona email no Header da aplicacao

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
       <>
-        <h2 data-testid="email-field">email:</h2>
+        <h2 data-testid="email-field">{ email }</h2>
         <ul>
           <li data-testid="total-field">0</li>
           <li data-testid="header-currency-field">BRL</li>
@@ -14,4 +17,12 @@ class Header extends Component {
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Header);
