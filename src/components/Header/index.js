462d40702502f25463539c1bd83f7557542ab356
Carlos Barros <carlos_dbs@hotmail.com>
2021-11-22 22:54:42 -0300

    Adiciona css componente Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import './Header.css';
+
 class Header extends Component {
   render() {
     const { email } = this.props;
     return (
-      <>
-        <h2 data-testid="email-field">{ email }</h2>
+      <header>
+        <h2>
+          <img src="/trybewallet-logo.jpeg" alt="logo trybe wallet" />
+        </h2>
+        <p data-testid="email-field">{`Email:  ${email}`}</p>
         <ul>
           <li data-testid="total-field">0</li>
           <li data-testid="header-currency-field">BRL</li>
         </ul>
-      </>
+      </header>
     );
   }
 }
