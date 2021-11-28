51a04af3f149f951b781492c75c245b8fdf103b9
Carlos Barros <carlos_dbs@hotmail.com>
2021-11-28 10:15:37 -0300

    Adiciona actionTypes para deletar e editar despesa

--- src/actions/actionsTypes.js
+++ src/actions/actionsTypes.js
@@ -2,6 +2,10 @@ export const LOGIN = 'LOGIN';
 
 export const ADD_EXPENSE = 'ADD_EXPENSE';
 
+export const DELETE_EXPENSE = 'DELETE_EXPENSE';
+
+export const EDIT_EXPENSE = 'EDIT_EXPENSE';
+
 export const GET_CURRENCIES = 'GET_CURRENCIES';
 
 export const GET_ERROR = 'GET_ERROR';
