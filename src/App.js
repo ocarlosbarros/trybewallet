c6f286ca84c72b36f3a228aee21e112323e34bf4
Carlos Barros <carlos_dbs@hotmail.com>
2023-08-25 15:57:32 -0300

    feat(App):adds 100vh to App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
+import './App.css';
+
 class App extends Component {
   render() {
     return (
